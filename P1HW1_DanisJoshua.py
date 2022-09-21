c6d19e8156e99a255b9bf08b76bc83b2aafba42a
JDanis165 <113880850+JDanis165@users.noreply.github.com>
2022-09-21 07:11:54 -0400

    Add files via upload
    Second Submission.

---
+++ P1HW1_DanisJoshua.py
@@ -0,0 +1,9 @@
+# Gathers User Input
+user_num = int(input("Enter integer:\n"))
+print("You entered:", user_num)
+print(user_num, "squared is", user_num * user_num)
+print("And", user_num, "cubed is", user_num * user_num * user_num,"!!")
+# Gathers Secondary Input 
+another_num = int(input("Enter another integer:\n"))
+print(user_num, "+", another_num, "is", user_num + another_num)
+print(user_num, "*", another_num, "is", user_num * another_num)
